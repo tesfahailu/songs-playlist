31ef1e9146f957b527c9492b3ea771df314511cc
Tesfa Hailu <>
2020-07-26 20:16:48 -0400

    fix: minor rearrangement of code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,14 @@
-import connectToDB from './db/sequelize';
+import connectToDataBase from './db/sequelize';
 import express from 'express';
 import 'dotenv/config';
 
 (async () => {
-  const port = process.env.SERVER_PORT;
-  connectToDB();
-
   const app = express();
-  app.get('/', (_req, res) => res.send('Hello World!'));
+  const port = process.env.SERVER_PORT;
 
+  connectToDataBase();
+
+  app.get('/', (_req, res) => res.send('Hello World!'));
   app.listen(port, () =>
     console.log(`Server listening at http://localhost:${port}`),
   );
