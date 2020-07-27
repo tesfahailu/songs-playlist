66fe5eca1e9093e3f9ed3c7688839e74445f24c7
Tesfa Hailu <>
2020-07-26 20:22:52 -0400

    fix: linking models to sequelize instance of database connection

--- server/src/db/sequelize.ts
+++ server/src/db/sequelize.ts
@@ -1,4 +1,9 @@
+import { Playlist } from './models/Playlist';
+import { Person } from './models/Person';
 import { Sequelize } from 'sequelize-typescript';
+import { PersonPlaylist } from './models/PersonPlaylist';
+import { PlaylistSong } from './models/PlaylistSong';
+import { Song } from './models/Song';
 
 export default async () => {
   const sequelize = await new Sequelize(process.env.SQL_CONNECTION_URL || '', {
@@ -6,12 +11,14 @@ export default async () => {
       timestamps: false,
     },
     dialect: 'postgres',
-    models: [__dirname + '/models'],
+    models: [Person, PersonPlaylist, Playlist, PlaylistSong, Song],
   });
 
   sequelize
     .authenticate()
-    .then(() => console.log('Data base connected successfully'))
+    .then(async () => {
+      console.log('Data base connected successfully');
+    })
     .catch((err: any) => console.log('Error', err));
 
   return sequelize;
