c52953caae6724b811a53da2f865b8163ade8cda
Tesfa Hailu <>
2020-07-26 20:11:42 -0400

    feat: create migration tools

---
+++ server/src/db/config.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+module.exports = {
+  development: {
+    use_env_variable: 'SQL_CONNECTION_URL',
+  },
+  production: {
+    use_env_variable: 'SQL_CONNECTION_URL',
+  },
+};
