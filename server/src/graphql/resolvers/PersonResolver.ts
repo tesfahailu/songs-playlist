4f2cc72a89b7a4a39f0e8ff1e301ae4b5c09ae13
Tesfa Hailu <>
2020-08-01 11:20:59 -0400

    feat: add person resolver to query person/people and to create, update, and delete person

--- server/src/graphql/resolvers/PersonResolver.ts
+++ server/src/graphql/resolvers/PersonResolver.ts
@@ -1,10 +1,125 @@
-import { Resolver, Query } from 'type-graphql';
+import {
+  Resolver,
+  Query,
+  Arg,
+  ArgsType,
+  Field,
+  Mutation,
+  Args,
+  ObjectType,
+} from 'type-graphql';
 import { Person } from './../../db/models/Person';
+
+@ArgsType()
+class CreatePersonArg {
+  @Field()
+  userName: string;
+
+  @Field()
+  password: string;
+
+  @Field({ nullable: true })
+  firstName?: string;
+
+  @Field({ nullable: true })
+  lastName?: string;
+
+  @Field({ nullable: true })
+  email?: string;
+
+  @Field({ nullable: true })
+  phoneNumber?: string;
+
+  @Field({ nullable: true })
+  birthDate?: Date;
+}
+
+@ObjectType()
+class DeletePersonResponse {
+  @Field()
+  success: Boolean;
+}
 
 @Resolver()
 export class PersonResolver {
+  @Query(() => Person)
+  async person(@Arg('userName') userName: string): Promise<Person | null> {
+    return await Person.findOne({ where: { userName } });
+  }
+
   @Query(() => [Person])
-  people() {
-    return Person.findAll();
+  async people(): Promise<Person[] | null> {
+    return await Person.findAll();
+  }
+
+  @Mutation(() => Person)
+  async createPerson(
+    @Args()
+    {
+      userName,
+      password,
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      birthDate,
+    }: CreatePersonArg,
+  ): Promise<Person | null> {
+    return await Person.create({
+      userName,
+      password,
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      birthDate,
+    });
+  }
+
+  @Mutation(() => Person)
+  async updatePerson(
+    @Args()
+    {
+      userName,
+      password,
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      birthDate,
+    }: CreatePersonArg,
+  ): Promise<Person | null> {
+    return (await Person.update(
+      {
+        userName,
+        password,
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        birthDate,
+      },
+      {
+        where: {
+          userName,
+        },
+      },
+    )) as any;
+  }
+
+  @Mutation(() => DeletePersonResponse)
+  async deletePerson(
+    @Arg('userName') userName: string,
+  ): Promise<DeletePersonResponse> {
+    const person = await Person.findOne({ where: { userName } });
+    if (!person) throw new Error('user does not exist');
+
+    try {
+      await Person.destroy({ where: { userName } });
+    } catch (err) {
+      console.log(err);
+      return { success: false };
+    }
+    return { success: true };
   }
 }
