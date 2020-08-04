46601f53e11d1ad9708831dfd5f67c351ca385ba
Tesfa Hailu <>
2020-08-04 09:30:02 -0400

    fix: model configuration

--- server/src/db/sequelize.ts
+++ server/src/db/sequelize.ts
@@ -1,13 +1,4 @@
-<<<<<<< Updated upstream
-import { Playlist } from './models/Playlist';
-import { Person } from './models/Person';
 import { Sequelize } from 'sequelize-typescript';
-import { PersonPlaylist } from './models/PersonPlaylist';
-import { PlaylistSong } from './models/PlaylistSong';
-import { Song } from './models/Song';
-=======
-import { Sequelize } from 'sequelize-typescript';
->>>>>>> Stashed changes
 
 export default async () => {
   const sequelize = await new Sequelize(process.env.SQL_CONNECTION_URL || '', {
